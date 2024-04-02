c704d72429f548100821150f1b694b1eb6ddfc1e
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-02 06:12:40 +0530

    feat: Add status

---
+++ lib/constants.ts
@@ -0,0 +1,18 @@
+import { ShortStatusMap, Tabs } from "./types";
+
+export const shortStatusMap: ShortStatusMap = {
+  Scheduled: ["TBD", "NS"],
+  InPlay: ["1H", "HT", "2H", "ET", "BT", "P", "SUSP", "INT", "LIVE"],
+  Finished: ["FT", "AET", "PEN"],
+  Postponed: ["PST"],
+  Cancelled: ["CANC"],
+  Abandoned: ["ABD"],
+  NotPlayed: ["AWD", "WO"],
+};
+
+export const tabs: Tabs[] = [
+  { status: "Scheduled", label: "Not Started" },
+  { status: "InPlay", label: "Live" },
+  { status: "Finished", label: "Finished" },
+  { status: "Postponed", label: "Postponed" },
+];
