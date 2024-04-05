5059a8dc936a6784d86e8eb29c66fde05aa2c46d
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-06 02:45:38 +0530

    refactor: Formatdatepattern func in utils for reusability

--- lib/utils.ts
+++ lib/utils.ts
@@ -100,3 +100,10 @@ export const getLeagues = (fixtures: Fixtures[]) => {
 
   return leagueInfo;
 };
+
+export const formatDatePattern = (date: Date) => {
+  const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+  return formattedDate;
+};
