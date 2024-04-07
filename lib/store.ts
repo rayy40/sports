4d62bb323157861c8d67b9c867a174a2ffae1d00
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-07 15:59:17 +0530

    chore: remove zustand

--- lib/store.ts
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-import { DetailedLeagueState, League } from "./types";
-
-export const useLeagueStore = create<DetailedLeagueState>((set) => ({
-  detailedLeague: null,
-  setDetailedLeague: (league) => set({ detailedLeague: league }),
-}));
