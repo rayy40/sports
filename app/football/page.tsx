524a9cb483c5f7352aa2a2ab34b2210e5da6de53
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-16 05:15:49 +0530

    style(UI): Style home page by changing the UI

--- app/football/page.tsx
+++ app/football/page.tsx
@@ -1,35 +1,131 @@
-import Sidebar from "@/components/ui/Sidebar";
-import Hero from "@/components/ui/Hero";
-import HomeFixtures from "@/components/ui/HomeFixtures";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-import React from "react";
-import { getCountries, getFixturesByDate } from "@/services/api";
+"use client";
 
-const Football = async () => {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["countries", "fixturesByDate"],
-    queryFn: () => {
-      getCountries();
-      getFixturesByDate("2024-03-03");
-    },
-  });
+import React, { useMemo, useState } from "react";
+import { useCountries, useFixturesByDate } from "@/services/football/queries";
+import {
+  filterDataByStatus,
+  formatDatePattern,
+  getItemsByLeague,
+  getLeagues,
+} from "@/lib/utils";
+import { BounceLoader } from "react-spinners";
+import BoxFixture from "@/components/ui/BoxFixture";
+import { StatusType } from "@/types/football";
+import { statusTabs } from "@/lib/constants";
+import Tabs from "@/components/ui/Tabs";
+import { FilterItems } from "@/components/ui/FilterItems";
+import DatePicker from "@/components/ui/DatePicker";
+import FootballLogo from "@/Assets/Logos/FootballLogo";
+import Link from "next/link";
+
+const Football = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const fixturesQuery = useFixturesByDate(formatDatePattern(date!));
+  const [status, setStatus] = useState<StatusType>("AllGames");
+  const [league, setLeague] = useState<string | null>(null);
+
+  const groupFixturesByStatus = useMemo(
+    () =>
+      fixturesQuery.data ? filterDataByStatus(fixturesQuery.data) : undefined,
+    [fixturesQuery.data]
+  );
+
+  const fixturesByLeague = useMemo(
+    () =>
+      groupFixturesByStatus
+        ? getItemsByLeague(groupFixturesByStatus[status])
+        : undefined,
+    [groupFixturesByStatus, status]
+  );
+
+  const leagueInfos = useMemo(
+    () => getLeagues(fixturesQuery.data!),
+    [fixturesQuery.data]
+  );
 
   return (
-    <div className="flex flex-row w-full h-screen font-sans gap-6 py-4">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="w-[15%] min-w-[220px]">
-          <Sidebar />
+    <div className="bg-background w-full min-h-screen font-sans">
+      <div className="px-6 border border-b shadow-sm sticky top-0 bg-background z-10">
+        <div className="py-6 flex justify-between items-center">
+          <h2 className="text-2xl flex items-center gap-3 text-secondary-foreground font-medium">
+            <FootballLogo width={30} height={30} />
+            Games
+          </h2>
+          <DatePicker date={date} setDate={setDate} />
         </div>
-        <div className="flex flex-col flex-1 gap-4 pr-4">
-          <Hero />
-          <HomeFixtures sport="football" />
+        <div className="flex items-end justify-between gap-4">
+          <div className="flex items-end gap-4">
+            {statusTabs.map((tab, index) => (
+              <Tabs<StatusType>
+                key={index}
+                label={tab.label}
+                id={tab.status}
+                status={status}
+                setStatus={setStatus}
+              />
+            ))}
+          </div>
+          <FilterItems
+            title={"Filter By: League"}
+            data={leagueInfos}
+            setFilter={setLeague}
+          />
         </div>
-      </HydrationBoundary>
+      </div>
+      {fixturesQuery.isFetching ? (
+        <div className="h-[calc(100vh-150px)] w-full flex items-center justify-center">
+          <BounceLoader color="hsl(45,89%,55%)" />
+        </div>
+      ) : (
+        <div className="h-[calc(100vh-150px)] overflow-y-auto px-6">
+          {fixturesByLeague && Object.keys(fixturesByLeague).length > 0 ? (
+            Object.keys(fixturesByLeague)
+              ?.filter((l) =>
+                league ? l.toLowerCase() === league.toLowerCase() : Boolean
+              )
+              ?.map((league, index) => {
+                const leagueId = fixturesByLeague[league][0].league.id;
+
+                return (
+                  <div key={index} className="space-y-2 py-10 border-b">
+                    <Link
+                      className="text-lg font-medium p-1 opacity-90 hover:opacity-100 transition-opacity"
+                      href={`/football/league/${leagueId}`}
+                    >
+                      {league}
+                    </Link>
+                    <div className="grid grid-cols-fixtures gap-6">
+                      {fixturesByLeague[league].map((fixture) => (
+                        <BoxFixture
+                          key={fixture.fixture.id}
+                          sport="football"
+                          round={fixture.league.round}
+                          id={fixture.fixture.id}
+                          date={fixture.fixture.date}
+                          status={fixture.fixture.status}
+                          homeTeam={{
+                            logo: fixture.teams.home.logo,
+                            name: fixture.teams.home.name,
+                          }}
+                          awayTeam={{
+                            logo: fixture.teams.away.logo,
+                            name: fixture.teams.away.name,
+                          }}
+                          homeTeamScore={fixture.goals.home}
+                          awayTeamScore={fixture.goals.away}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                );
+              })
+          ) : (
+            <div className="flex items-center justify-center h-[calc(100vh-150px)] w-full">
+              <p>No fixtures found.</p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
