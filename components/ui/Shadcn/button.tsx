7a8ed8e009ea2c8618bda4edea69e52530674f44
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-16 05:22:58 +0530

    style: add style for ghost variant

--- components/ui/Shadcn/button.tsx
+++ components/ui/Shadcn/button.tsx
@@ -13,10 +13,10 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background text-tertiary-foreground hover:bg-tertiary hover:text-accent-foreground",
+          "border border-input bg-background text-tertiary-foreground hover:bg-tertiary hover:text-tertiary-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-card/70 hover:text-card-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
