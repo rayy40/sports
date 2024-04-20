f4a9dc45a89a2ca1956b8974f2d4d56f8b929863
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-21 04:00:57 +0530

    feat: add rugby types

---
+++ types/rugby.ts
@@ -0,0 +1,17 @@
+import { GamesStats, WinOrLose } from "./general";
+
+export interface RugbyScores {
+  home: number | null;
+  away: number | null;
+}
+
+export interface Periods {
+  first: RugbyScores;
+  second: RugbyScores;
+  overtime: RugbyScores;
+  secondt_overtime: RugbyScores;
+}
+
+export interface RugbyGameStats extends GamesStats {
+  draw: WinOrLose;
+}
