0f643c22603f9263699d62a876afc16baf221a0d
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-21 04:04:15 +0530

    feat: add store

---
+++ lib/store.ts
@@ -0,0 +1,79 @@
+import { DetailedTabsType, StatusType } from "@/types/general";
+import { create } from "zustand";
+
+const dt = new Date();
+
+export interface StatusStore {
+  status: StatusType;
+  setStatus: (arg: StatusType) => void;
+}
+
+export interface TabsStore {
+  tab: DetailedTabsType;
+  setTab: (arg: DetailedTabsType) => void;
+}
+
+export interface StatStore {
+  stat: string;
+  setStat: (arg: string) => void;
+}
+
+export interface DateStore {
+  date: Date;
+  setDate: (date: Date | undefined) => void;
+}
+
+export interface LeagueStore {
+  league: string | null;
+  setLeague: (league: string | null) => void;
+}
+
+export interface TeamStore {
+  team: string | null;
+  setTeam: (team: string | null) => void;
+}
+
+export interface SeasonsStore {
+  season: string | null;
+  setSeason: (season: string | null) => void;
+}
+
+export const useStatusStore = create<StatusStore>((set) => ({
+  status: "AllGames",
+  setStatus: (arg: StatusType) => set({ status: arg }),
+}));
+
+export const useTabsStore = create<TabsStore>((set) => ({
+  tab: "Fixtures",
+  setTab: (arg: DetailedTabsType) => set({ tab: arg }),
+}));
+
+export const useStatStore = create<StatStore>((set) => ({
+  stat: "top scorers",
+  setStat: (arg: string) => set({ stat: arg }),
+}));
+
+export const useDateStore = create<DateStore>((set) => ({
+  date: dt,
+  setDate: (date: Date | undefined) => set({ date }),
+}));
+
+export const useLeagueStore = create<LeagueStore>((set) => ({
+  league: null,
+  setLeague: (league: string | null) => set({ league }),
+}));
+
+export const useLeagueForTeamStatsStore = create<LeagueStore>((set) => ({
+  league: null,
+  setLeague: (league: string | null) => set({ league }),
+}));
+
+export const useTeamStore = create<TeamStore>((set) => ({
+  team: null,
+  setTeam: (team: string | null) => set({ team }),
+}));
+
+export const useSeasonsStore = create<SeasonsStore>((set) => ({
+  season: null,
+  setSeason: (season: string | null) => set({ season }),
+}));
