540a075a8968eaa366cc4d4cd2623f87223c2e8e
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-24 03:01:03 +0530

    chore: add hockey events types

--- types/hockey.ts
+++ types/hockey.ts
@@ -1,4 +1,4 @@
-import { GamesStats, WinOrLose } from "./general";
+import { GamesStats, WinOrLose, Team } from "./general";
 
 export interface Periods {
   first: string | null;
@@ -11,4 +11,15 @@ export interface Periods {
 export interface HockeyGameStats extends GamesStats {
   win_overtime: WinOrLose;
   lose_overtime: WinOrLose;
+}
+
+export interface HockeyEvents {
+  game_id: number;
+  period: string;
+  minute: string;
+  team: Team;
+  players?: string[] | null;
+  assists?: (string | null)[] | null;
+  comment?: string | null;
+  type: string;
 }
