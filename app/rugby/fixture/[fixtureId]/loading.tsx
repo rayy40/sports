315a860b7e5c0e946078aa5ff4334ca979ea18a2
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-23 14:26:20 +0530

    feat: add fixture loading

---
+++ app/rugby/fixture/[fixtureId]/loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BounceLoading from "@/components/Loading";
+
+const Loading = () => {
+  return (
+    <div className="w-full h-screen">
+      <BounceLoading />
+    </div>
+  );
+};
+
+export default Loading;
