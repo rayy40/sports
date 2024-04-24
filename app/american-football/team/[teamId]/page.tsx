77eb209b0423c4a4beffb3c074003392f6a83867
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-24 13:43:35 +0530

    feat: add NFL team page

---
+++ app/american-football/team/[teamId]/page.tsx
@@ -0,0 +1,67 @@
+import { Team } from "@/types/general";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { getFixturesByTeamIdAndSeason, getTeamById } from "@/services/api";
+import LeagueOrTeamWrapper from "@/components/LeagueOrTeamWrapper";
+import { NFLGames } from "@/types/american-football";
+import { Seasons } from "@/lib/constants";
+
+const Page = async ({ params }: { params: { teamId: string } }) => {
+  const teamId = parseInt(params.teamId);
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: [teamId, "american-football", "team"],
+    queryFn: () => getTeamById(teamId, "american-football"),
+  });
+
+  const team: Team | undefined = queryClient.getQueryData([
+    teamId,
+    "american-football",
+    "team",
+  ]);
+
+  if (!team) {
+    return (
+      <div className="flex font-sans text-[1rem] h-screen w-full items-center justify-center">
+        <p>No team found.</p>
+      </div>
+    );
+  }
+
+  const season = "2023";
+
+  await queryClient.prefetchQuery({
+    queryKey: [teamId, season, "american-football", "fixtures"],
+    queryFn: () =>
+      getFixturesByTeamIdAndSeason(teamId, season, "american-football"),
+  });
+
+  const fixtures: NFLGames[] | undefined = queryClient.getQueryData([
+    teamId,
+    season,
+    "american-football",
+    "fixtures",
+  ]);
+
+  return (
+    <div className="relative font-sans">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <LeagueOrTeamWrapper
+          title={team?.name}
+          logo={team?.logo}
+          isTeam={true}
+          id={team.id}
+          seasons={Seasons}
+          sport="american-football"
+          currSeason={season}
+          fixtures={fixtures}
+        />
+      </HydrationBoundary>
+    </div>
+  );
+};
+
+export default Page;
