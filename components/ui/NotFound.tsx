e38ed87e74d18f43bc7753e681cade8fc4c3a705
Rayyan Alam <rayyanalam650@gmail.com>
2024-04-29 03:15:16 +0530

    fix: made sports optional

--- components/ui/NotFound.tsx
+++ components/ui/NotFound.tsx
@@ -2,9 +2,9 @@ import { Sports } from "@/types/general";
 import Link from "next/link";
 import React from "react";
 
-const NotFound = ({ type, sport }: { type: string; sport: Sports }) => {
+const NotFound = ({ type, sport }: { type: string; sport?: Sports }) => {
   return (
-    <div className="h-screen w-full flex items-center justify-center">
+    <div className="flex items-center justify-center w-full h-screen">
       <p>No {type} found.</p>
       <Link href={`/${sport}`} className="underline-hover">
         Go back
