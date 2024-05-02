9e75360e2c167d631d45c8d6f485b66256ab6d0c
Rayyan Alam <rayyanalam650@gmail.com>
2024-05-03 03:44:46 +0530

    fix: error handling in AFL

--- app/australian-football/page.tsx
+++ app/australian-football/page.tsx
@@ -6,7 +6,6 @@ import { getFixturesByDate } from "@/services/api";
 import { format } from "date-fns";
 import DatePicker from "@/components/ui/DatePicker";
 import ErrorBoundary from "@/components/Error";
-import { AllSportsFixtures } from "@/types/general";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -21,10 +20,31 @@ const getFixture = cache(async (date: string) => {
 const Page = async () => {
   const date = new Date();
   const formattedDate = format(date, "yyyy-MM-dd");
-  let fixtures: AllSportsFixtures[] | null | undefined = undefined;
 
   try {
-    fixtures = await getFixture(formattedDate);
+    const fixtures = await getFixture(formattedDate);
+
+    return (
+      <div className="flex flex-col w-full h-screen font-sans bg-background">
+        <div className="sticky top-0 z-10 px-3 border border-b shadow-sm lg:px-6 bg-background">
+          <div className="flex items-center justify-between py-3 lg:py-6">
+            <h2 className="flex items-center gap-2 text-xl font-medium lg:gap-3 lg:text-2xl text-secondary-foreground">
+              <NFL width={50} height={50} />
+              Games
+            </h2>
+            <DatePicker />
+          </div>
+          <FilterWrapper
+            fixtures={fixtures ?? []}
+            isHome={true}
+            sport={"australian-football"}
+          />
+        </div>
+        <div className="flex-1 overflow-y-auto">
+          <HomeWrapper sport={"australian-football"} />
+        </div>
+      </div>
+    );
   } catch (error) {
     return (
       <div className="w-full h-screen">
@@ -32,36 +52,6 @@ const Page = async () => {
       </div>
     );
   }
-
-  if (!fixtures) {
-    return (
-      <div className="h-screen text-sm lg:text-[1rem] w-full flex items-center justify-center">
-        <p>No fixtures found.</p>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col w-full h-screen font-sans bg-background">
-      <div className="sticky top-0 z-10 px-3 border border-b shadow-sm lg:px-6 bg-background">
-        <div className="flex items-center justify-between py-3 lg:py-6">
-          <h2 className="flex items-center gap-2 text-xl font-medium lg:gap-3 lg:text-2xl text-secondary-foreground">
-            <NFL width={50} height={50} />
-            Games
-          </h2>
-          <DatePicker />
-        </div>
-        <FilterWrapper
-          fixtures={fixtures}
-          isHome={true}
-          sport={"australian-football"}
-        />
-      </div>
-      <div className="flex-1 overflow-y-auto">
-        <HomeWrapper sport={"australian-football"} />
-      </div>
-    </div>
-  );
 };
 
 export default Page;
