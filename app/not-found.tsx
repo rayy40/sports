0444c9138dd3d41488bca4a4b7c98a058b7547e6
Rayyan Alam <rayyanalam650@gmail.com>
2024-05-09 00:40:24 +0530

    style: not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen gap-2 font-sans">
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link className="underline-hover" href="/">
+      <h2 className="font-medium text-lg">Not Found</h2>
+      <Link
+        className="underline-hover text-secondary-foreground hover:text-foreground"
+        href="/"
+      >
         Go Home
       </Link>
     </div>
