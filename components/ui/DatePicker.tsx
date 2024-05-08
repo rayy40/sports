4329056c4cfd4e60775009f7fa252d26f7dd34b7
Rayyan Alam <rayyanalam650@gmail.com>
2024-05-08 17:48:40 +0530

    fix: date picker to change date using query params

--- components/ui/DatePicker.tsx
+++ components/ui/DatePicker.tsx
@@ -4,26 +4,47 @@ import { Calendar as CalendarIcon } from "lucide-react";
 import { Button } from "./Shadcn/button";
 import { Calendar } from "./Shadcn/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "./Shadcn/popover";
-import { formatDatePattern } from "@/lib/utils";
-import { useDateStore } from "@/lib/store";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { format, parseISO } from "date-fns";
 
-const DatePicker = () => {
-  const { date, setDate } = useDateStore();
+const DatePicker = ({ date }: { date: string }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const handleSelect = (currentValue?: Date) => {
+    if (!currentValue) return;
+
+    const current = new URLSearchParams(searchParams);
+    const value = format(currentValue, "yyyy-MM-dd");
+
+    current.set("date", value);
+
+    const search = current.toString();
+    const query = search ? `?${search}` : "";
+
+    router.push(`${pathname}${query}`);
+  };
+
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
           variant={"outline"}
           className={
-            "w-[140px] h-9 text-sm lg:w-[200px] lg:h-10 justify-center text-center font-medium"
+            "w-[140px] h-9 text-sm lg:w-[160px] lg:h-10 justify-center text-center font-medium"
           }
         >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? formatDatePattern(date) : <span>Pick a date</span>}
+          <CalendarIcon className="w-4 h-4 mr-2" />
+          {date}
         </Button>
       </PopoverTrigger>
       <PopoverContent align="end" className="w-auto p-0 font-sans">
-        <Calendar mode="single" selected={date} onSelect={setDate} />
+        <Calendar
+          mode="single"
+          selected={parseISO(date)}
+          onSelect={(value) => handleSelect(value)}
+        />
       </PopoverContent>
     </Popover>
   );
