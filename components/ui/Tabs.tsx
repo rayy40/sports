a8157ef640970927ff8570da59f0671c09a3b39d
Rayyan Alam <rayyanalam650@gmail.com>
2024-05-08 17:49:21 +0530

    fix: add support for status

--- components/ui/Tabs.tsx
+++ components/ui/Tabs.tsx
@@ -7,14 +7,15 @@ type Props = {
   id: string;
   leagueId?: string;
   isTeam?: boolean;
+  isHome?: boolean;
 };
 
-const Tabs = ({ id, leagueId, isTeam = false }: Props) => {
+const Tabs = ({ id, leagueId, isTeam = false, isHome = false }: Props) => {
   const path = usePathname();
   const searchParams = useSearchParams();
 
   const handleSearchParams = (title: string, value?: string) => {
-    if (!isTeam || !value) return;
+    if (!value) return;
     const current = new URLSearchParams(searchParams);
 
     current.set(title, value);
@@ -26,20 +27,27 @@ const Tabs = ({ id, leagueId, isTeam = false }: Props) => {
   };
 
   const baseURL = getBaseUrl(path);
+  const status = searchParams.get("status");
   const segments = path.split("/");
   const lastSegment = segments[segments.length - 1];
 
   return (
     <Link
       href={{
-        pathname: `${baseURL}/${id.toLowerCase().replaceAll(/\s/g, "-")}`,
-        query: handleSearchParams("league", leagueId?.toLowerCase()),
+        pathname: isHome
+          ? path
+          : `${baseURL}/${id.toLowerCase().replaceAll(/\s/g, "-")}`,
+        query: handleSearchParams(
+          isHome ? "status" : "league",
+          isHome ? id.toLowerCase() : leagueId?.toLowerCase()
+        ),
       }}
       className={cn(
         "p-[10px] hidden lg:inline-block text-sm font-medium whitespace-nowrap transition-all cursor-pointer text-muted-foreground hover:text-primary-foreground/95",
         {
           "text-primary-foreground/95 underline-tabs":
-            id.toLowerCase().replaceAll(/\s/g, "-") === lastSegment,
+            id.toLowerCase().replaceAll(/\s/g, "-") ===
+            (isHome ? status?.replaceAll(/\s/g, "-") : lastSegment),
         }
       )}
     >
